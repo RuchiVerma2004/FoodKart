868ae208c4ebbc2e882625df94bcc592d954052c
RuchiVerma2004 <verma.ruchi0123@gmail.com>
2024-06-24 01:40:29 +0530

    changes

--- app.js
+++ app.js
@@ -38,7 +38,16 @@ const Header = () =>{
 const RestaurentCard = ()=>{
     return (
         <div className="restaurent-card">
-            
+            <img className="card-img" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e0839ff574213e6f35b3899ebf1fc597" alt="ing not found"/>
+            <h3 className="restaurent-name">Chinese Wok</h3>
+            <p className="cuisine">Chinese, Asian, Tibetan, Desserts</p>
+            <div className="detail">
+                <p className="rating"> 4.3</p>
+                <p className="time">40-45 mins</p>
+                <p className="price">365</p>
+            </div>
+            <hr/>
+            <p className="offer">30% Off on order above 1000</p>
         </div>
     );
 };
@@ -48,7 +57,9 @@ const Body = () =>{
         <div className="body">
             <div className="Search">Search</div>
             <div className="Restaurent-Container">
-                <RestaurentCard />
+                <RestaurentCard /><RestaurentCard /><RestaurentCard /><RestaurentCard /><RestaurentCard />
+                <RestaurentCard /><RestaurentCard /><RestaurentCard /><RestaurentCard /><RestaurentCard /><RestaurentCard />
+                <RestaurentCard /><RestaurentCard /><RestaurentCard /><RestaurentCard /><RestaurentCard /><RestaurentCard /><RestaurentCard /><RestaurentCard /><RestaurentCard /><RestaurentCard /><RestaurentCard /><RestaurentCard /><RestaurentCard />
             </div>
         </div>
     );
@@ -58,7 +69,7 @@ const AppLayout = ()=>{
     return (
         <div className="app">
             <Header/>
-
+            <Body/>
         </div>
     );
 };
