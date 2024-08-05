60388cff5b04de8bd0248b8d6272d00412d42f2b
RuchiVerma2004 <verma.ruchi0123@gmail.com>
2024-08-05 16:49:48 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -1,24 +1,40 @@
-import React from "react";
+import React, { lazy, Suspense, useEffect, useState} from "react";
+
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Contact from "./components/Contact";
 import About from "./components/About.js";
 import ResMenue from "./components/ResMenue.js";
+import Error from "./components/Error.js";
 // import restaurants from "./utils/mockdata";
 import { createBrowserRouter, Outlet, RouterProvider} from "react-router-dom";
-
+import UserContext from "./utils/UserContext";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 
 
 const AppLayout = ()=>{
 
+    //authentication
+    useEffect(()=>{
+        const data = {
+            name: "Ruchi Verma",
+        };
+        setUserName(data.name);
+    }, []);
+
     console.log("Applayout");
     return (
-        <div className="app">
+        <Provider store={appStore}>
+            <UserContext.Provider value={{loggedInUser: userName, setUserName }}>
+                <div className="app">
 
-            <Header/>
-            <Outlet/>
-        </div>
+                    <Header/>
+                    <Outlet/>
+                </div>
+            </UserContext.Provider>
+        </Provider>
     );
 };
 
