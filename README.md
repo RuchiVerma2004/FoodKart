20abb7bb54db8eb002fb5e84cc00d62873864d92
RuchiVerma2004 <verma.ruchi0123@gmail.com>
2024-08-11 23:12:05 +0530

    change

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 Food Ordering Website Build using React
 
 here in the code for the purpose of learning how induatry works with api we have use swigy api if in case there is any update in swiggy api and it is not working for this application use mockData.js file and do needfull changes as mentioned in Body component your code will start working
+
+# Setting up Testing in our app
+- install React Testing Library
+- Install jest
+- Install Babel dependencies 
+- Configue Babel 
+- Configue Parcel Comfig file to disable default babel transpilation
+- jest - npx jest --init
+- Install jsdom library
+
+
--- src/utils/cartSlice.js
+++ src/utils/cartSlice.js
@@ -7,14 +7,18 @@ const  cartSlice = createSlice({
         },
         reducers:{
             addItem:(state, action)=>{
-                // Vanilla(older) Redux => DON'T MUTATAE STATE
+                // Vanilla(older) Redux => DON'T MUTATAE STATE and returning was a kind of mandatory now returning is not manda tory
                 // const newState = [...state];
                 // newState.items.push(action.payload);
                 // return newState;
 
+
                 // Resux toolkit 
                 // WE Have to mutate the state
-                
+
+                // redux toolkit uses immer library Behind the scene
+                // 
+
                 state.items.push(action.payload);
             },
             removeItem:(state, action) => {
