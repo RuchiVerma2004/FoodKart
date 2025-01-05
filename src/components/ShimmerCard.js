bf6ed9639426b3bdac3a01a19e328b20ac008be9
Ruchi Verma <156408077+RuchiVerma2004@users.noreply.github.com>
2025-01-05 17:57:06 +0530

    Update ShimmerCard.js

--- src/components/ShimmerCard.js
+++ src/components/ShimmerCard.js
@@ -1,16 +1,27 @@
 const ShimmerCard = ()=>{
-    return <div className="">
-            <div className=""  ></div>
-            <p className=""></p>
-            <p className=""></p>
-            <div className="">
-                <p className=""></p>
-                <p className=""></p>
-                <p className=""></p>
+    return (
+        <div className="restaurent-card w-64 rounded-2xl p-2 m-4 bg-white flex flex-col justify-between">
+          <div className="rounded-sm animate-pulse">
+            <div className="rounded-2xl w-full h-36 bg-gray-200"></div>
+          </div>
+          <div className="px-4 animate-pulse">
+         
+            <div className="mt-5 h-6 bg-gray-200 rounded-md w-3/4"></div>
+            
+            <div className="flex justify-between my-2"></div>
+    
+            <div className="text-gray-600">
+              <div className="h-5 bg-gray-200 rounded-md w-full mb-2"></div>
+              <div className="h-5 bg-gray-200 rounded-md w-full mb-2"></div>
+              <div className="h-5 bg-gray-200 rounded-md w-full mb-2"></div>
+            
             </div>
-            <p className=""></p>
-           
+    
+            
+
+          </div>
         </div>
-}
+      );
+};
 
 export default ShimmerCard;
